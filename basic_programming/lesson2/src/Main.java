6848ae21f71943a3e8b7c2939641b34be3330c65
EvgenyGroup <mailfrom61@gmail.com>
2023-01-18 21:26:15 +0100

    lesson2

---
+++ basic_programming/lesson2/src/Main.java
@@ -0,0 +1,109 @@
+public class Main {
+// this is comments 1
+//    rte rtzuj
+    /* sdbgsdhftzfgkhuli
+    * ydvdgfbgnhgmjh,j,*/
+    public static void main(String[] args) {
+
+        int sumOfTwIntegers;
+
+
+        String b;
+
+        // имя = значение
+        int num = 1;
+        int num1 = 2;
+        b="Hello World";
+        int c =2;
+       // a=a+1;
+       // c=a+c;
+
+    //    System.out.print("Hello world!");
+    //    System.out.println("Hello everyone!");
+
+        String str = "Evgeny";
+        String strInt = "123";
+        int num2=123;
+        String str2 = "Evg"+"eny";
+        String str3 = str +strInt;
+        String str4 = str +" "+strInt;
+
+     //   System.out.println(str);
+        System.out.println("in str2 is "+str2);
+        System.out.println("in str3 is "+ str3);
+        System.out.println("in str4 is "+str4);
+
+        // Name: Evgeny
+        // Surname: Grazhdanskiy
+
+        String name= "Valentin";
+        String secondName= "Dumler";
+        int age=49;
+        String ageStr = "49";
+
+        System.out.println("My name: "+name);
+        System.out.println("My second name: "+secondName);
+        System.out.println("my age is "+age);
+
+        int x = 10;
+        x=x+1;
+        x+=1;
+
+        x=x-100;
+        x-=100;
+        x=-100;
+
+
+
+        String abc = "abc";
+     //   abc=23;
+
+        int y = 10;
+      //  y="werzt";
+      //  int result;
+        String result;
+      //  result =10+10;
+
+        System.out.println(abc+y);
+        System.out.println(y+y);
+        System.out.println("sum of 10+10= "+(10+10));
+        System.out.println(10+"10");
+        System.out.println(10+" "+10);
+
+        int z = 5;
+        System.out.println(z);
+        int y1=z+2;
+        y=z+2;
+
+         z=6;
+        System.out.println(z);
+
+        z=z+1;
+        System.out.println(z);
+        y=z+2;
+        System.out.println(y);
+
+    //посчитать сумму двух целых чисел, задав предварительно три переменные
+        // 1 число, 2 число, 3 число это результат
+        // и вывести значение результата на экран
+        /* jjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+
+         */
+        /* Закомментировать лишние строчки чтобы вывести на экран
+        one plus two equals 3
+        int a = 1;
+        int b = 2;
+        System.out.print("one");
+        System.out.print(b);
+        System.out.print(" plus ");
+        System.out.print(" minus ");
+        System.out.print(a);
+        System.out.print("two");
+        System.out.print(" equals ");
+        System.out.print(" three ");
+        System.out.print(a+b);
+         */
+
+
+    }
+}
